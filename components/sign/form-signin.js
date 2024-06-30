1bbd8b53a690375634f086c4ebbbe4a654411123
Dindin Imanudin <dindin.imanudin@mhs.itenas.ac.id>
2024-06-30 19:38:08 +0700

    perbaikan

--- components/sign/form-signin.js
+++ components/sign/form-signin.js
@@ -16,7 +16,7 @@ export function Form() {
             <RiScissors2Fill size={50} className="m-auto"/>
             <h2 className="text-3xl my-10 text-center font-bold">Sign In</h2>
             <input required disabled={isPending} className="border-2 text-lg py-1 px-4 border-solid border-black rounded-full placeholder-slate-900 outline-none block my-4 font-bold w-full" name="email" onChange={handleChange} placeholder="Email" type="email"/>
-            <input required disabled={isPending} className="border-2 py-1 px-4 border-solid border-black rounded-full placeholder-slate-900 outline-none block my-4 text-lg font-bold w-full" name="email" onChange={handleChange} placeholder="Password" type="password"/>
+            <input required disabled={isPending} className="border-2 py-1 px-4 border-solid border-black rounded-full placeholder-slate-900 outline-none block my-4 text-lg font-bold w-full" name="password" onChange={handleChange} placeholder="Password" type="password"/>
             <Submit name="Sign In"/>
             <p className="text-sm mt-6 text-center">don't have an account? <Link className="text-blue-700 font-black" href="sign-up">Sign Up</Link>
             </p>
